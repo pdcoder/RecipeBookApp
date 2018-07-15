a8cedb79b386085e512c66780944d798dafb90d9
Prakash <prakashdas1998.pd@gmail.com>
2018-07-15 18:56:18 +0530

    Fixed Recipes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'app';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/recipes/recipes.services.ts
+++ src/app/recipes/recipes.services.ts
@@ -1,6 +1,6 @@
 import {Recipe} from  './recipe.model';
 export class Recipes{
-    recipes : Recipe[] = [
+    private recipes : Recipe[] = [
         new Recipe('A Test Recipe','This is a simple  test','https://img.bestrecipes.com.au/rZFo7F8i/h300-w400-cscale-1495077669/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
       ];
 }
