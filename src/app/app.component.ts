36b5eb375acc83af4e15207fd3d0aebc02361e49
Prakash <prakashdas1998.pd@gmail.com>
2018-07-26 19:47:49 +0530

    Added authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   loadedFeature = 'recipe';
 
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyCQpFkhEfzhEY3FoOMCBzstkBWvrADvXp8",
+      authDomain:"recipebook-6fa7f.firebaseapp.com"
+    });
+  }
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
