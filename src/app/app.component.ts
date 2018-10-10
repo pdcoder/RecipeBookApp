e1dfc5000bc8bb4f0ca3f4b4893ab70994406f5b
Prakash <prakashdas1998.pd@gmail.com>
2018-10-10 22:47:02 +0530

    Finished app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     firebase.initializeApp({
-      apiKey: "AIzaSyDGDTtGNC4FkKpSe85qb2wLHzNi2xkxFy8",
-      authDomain: "ng-recipe-book.firebaseapp.com"
+      apiKey: "AIzaSyCQpFkhEfzhEY3FoOMCBzstkBWvrADvXp8",
+      authDomain: "recipebook-6fa7f.firebaseapp.com"
     });
   }
 
